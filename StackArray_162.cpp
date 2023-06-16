4c7645a89205351a8f89f337b62565824a92e35a
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-06-16 11:10:54 +0700

    int push(int element)

--- StackArray_162.cpp
+++ StackArray_162.cpp
@@ -8,13 +8,24 @@ private:
     int stack_Array[5];
     int top;
 public:
+    //constructor
+    stackArray() {
+        top = -1;
+    }
+
+    int push(int element) {
+        if (top == 4) {
+            cout << "Number of data exceeds the limit." << endl;
+            return 0;
+        }
+
+        top++;
+        stack_Array[top] = element; //step3
+        cout << endl;
+        cout << element << "Ditambahkan(pushed)" << endl;
+
+        return element;
+   
+    }
     
-};
-
-
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+    
