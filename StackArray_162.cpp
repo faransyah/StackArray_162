9d33942361ea916e196f1a95a28c890b73407fc9
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-06-16 10:46:57 +0700

    Add project files.

---
+++ StackArray_162.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class stackArray {
+private:
+    int stack_Array[5];
+    int top;
+public:
+
+};
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
